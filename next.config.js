7fbb070930fea3ebe51d974c7c547a4dc15bd418
WayanPande <yande554@gmail.com>
2024-05-01 19:39:27 +0800

    fix: asset prefix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withNextra = require("nextra")({
 });
 
 const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "/onboarding" : "";
+const assetPrefix = isProduction ? "/test-docs" : "";
 
 module.exports = withNextra({
   reactStrictMode: true,
